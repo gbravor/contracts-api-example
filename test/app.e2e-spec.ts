28edcbc3d5a263754435fcadd4c62d6a5fff5e57
Gabriel Bravo <gbravor01@gmail.com>
2024-07-29 09:06:39 -0400

    test: add contracts e2e tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,9 +2,14 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { ContractRequest } from '../src/contracts/application/ContractRequest.dto';
+import { ContractsService } from '../src/contracts/application/contracts.service';
+import { PrismaService } from '../src/prisma.service';
 
-describe('AppController (e2e)', () => {
+describe('App (e2e)', () => {
   let app: INestApplication;
+  let contractsService;
+  let prisma: PrismaService;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -12,13 +17,38 @@ describe('AppController (e2e)', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    contractsService = moduleFixture.get(ContractsService);
+    prisma = moduleFixture.get(PrismaService);
+
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  afterAll(async () => {
+    await prisma.$executeRaw`TRUNCATE TABLE "Contract" RESTART IDENTITY CASCADE;`;
+  });
+
+  describe('Contracts', () => {
+    it('should create contract (POST)', async () => {
+      const contract = await contractsService.create(payload);
+
+      expect(contract.clientName).toBe(payload.clientName);
+      expect(contract.currency).toBe(payload.currency);
+    });
+  });
+
+  it('should get contracts (GET)', () => {
     return request(app.getHttpServer())
-      .get('/')
+      .get('/contractList')
       .expect(200)
-      .expect('Hello World!');
+      .expect([{ id: 1, ...payload }]);
   });
 });
+
+const payload: ContractRequest = {
+  clientName: 'Joe',
+  email: 'joe@gmail.com',
+  initialDate: '07-19-2024',
+  accountNumber: '439040',
+  amount: 2000,
+  currency: 'USD',
+};
