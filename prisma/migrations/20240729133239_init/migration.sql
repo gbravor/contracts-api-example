c8ff6aeb9e249b4e9ce736c430c595034174ed63
Gabriel Bravo <gbravor01@gmail.com>
2024-07-29 09:35:12 -0400

    chore: add validator dependency·

--- prisma/migrations/20240729133239_init/migration.sql
+++ prisma/migrations/20240729133239_init/migration.sql
@@ -3,9 +3,9 @@ CREATE TABLE "Contract" (
     "id" SERIAL NOT NULL,
     "clientName" TEXT NOT NULL,
     "email" TEXT NOT NULL,
-    "initialDate" TIMESTAMP(3) NOT NULL,
-    "accountNumber" INTEGER NOT NULL,
-    "amount" BIGINT NOT NULL,
+    "initialDate" TEXT NOT NULL,
+    "accountNumber" TEXT NOT NULL,
+    "amount" INTEGER NOT NULL,
     "currency" TEXT NOT NULL,
 
     CONSTRAINT "Contract_pkey" PRIMARY KEY ("id")
