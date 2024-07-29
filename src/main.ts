6c7baefefea97b16cb20905d348f6e56baf31181
Gabriel Bravo <gbravor01@gmail.com>
2024-07-29 09:04:54 -0400

    feat: implement swagger docs

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-
+  const config = new DocumentBuilder()
+    .setTitle('Challenge MS')
+    .setDescription(
+      'Challenge to test knowledge about microservices for the use of REST architecture',
+    )
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
